6c07c0f96b530dcb156b0efb5b0ee477fa1d25dc
gramcracker <watermelonz656@gmail.com>
2023-02-15 23:56:22 -0600

    Temporarily commented out example code. Added function to save list of tab urls into a map, were the key is a timestamp.

--- popup.js
+++ popup.js
@@ -12,40 +12,56 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const tabs = await chrome.tabs.query({
-  url: [
-    "https://*",
-    "https://*/*",
-  ],
-});
-
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/Collator
-const collator = new Intl.Collator();
-tabs.sort((a, b) => collator.compare(a.title, b.title));
-
-const template = document.getElementById("li_template");
 const elements = new Set();
-for (const tab of tabs) {
-  const element = template.content.firstElementChild.cloneNode(true);
+let map = new Map();
 
-  const title = tab.title.split("-")[0].trim();
-  const pathname = new URL(tab.url).pathname.slice("/docs".length);
-
-  element.querySelector(".title").textContent = title;
-  element.querySelector(".pathname").textContent = pathname;
-  element.querySelector("a").addEventListener("click", async () => {
-    // need to focus window as well as the active tab
-    await chrome.tabs.update(tab.id, { active: true });
-    await chrome.windows.update(tab.windowId, { focused: true });
-  });
-
-  elements.add(element);
+function getTabs(tabs) {
+  let timestamp = new Date().getTime();
+  let session = [];
+  for (const tab of tabs) {
+    session.push(tab.url);
+    console.log(tab.url);
+    console.log(timestamp);
+  }
+  map.set(timestamp, session);
 }
-document.querySelector("ul").append(...elements);
 
-const button = document.querySelector("button");
-button.addEventListener("click", async () => {
-  const tabIds = tabs.map(({ id }) => id);
-  const group = await chrome.tabs.group({ tabIds });
-  await chrome.tabGroups.update(group, { title: "DOCS" });
-});
+function onError(error) {
+  console.error(`Error: ${error}`);
+}
+
+chrome.tabs.query({}).then(getTabs, onError);
+
+
+// // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/Collator
+// const collator = new Intl.Collator();
+// _tabs.sort((a, b) => collator.compare(a.title, b.title));
+
+// const template = document.getElementById("li_template");
+
+
+
+// for (const tab of tabs) {
+//   const element = template.content.firstElementChild.cloneNode(true);
+
+//   const title = tab.title.split("-")[0].trim();
+//   const pathname = new URL(tab.url).pathname.slice("/docs".length);
+
+//   element.querySelector(".title").textContent = title;
+//   element.querySelector(".pathname").textContent = pathname;
+//   element.querySelector("a").addEventListener("click", async () => {
+//     // need to focus window as well as the active tab
+//     await chrome.tabs.update(tab.id, { active: true });
+//     await chrome.windows.update(tab.windowId, { focused: true });
+//   });
+
+//   elements.add(element);
+// }
+// document.querySelector("ul").append(...elements);
+
+// const button = document.querySelector("button");
+// button.addEventListener("click", async () => {
+//   const tabIds = tabs.map(({ id }) => id);
+//   const group = await chrome.tabs.group({ tabIds });
+//   await chrome.tabGroups.update(group, { title: "DOCS" });
+// });
