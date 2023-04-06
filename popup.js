0a994df6e0dabd7058a62bfb6710819c333ddbc8
paola <paolapolvera@gmail.com>
2023-04-05 22:46:29 -0500

    Added styling to save tab list, and made some files up to date with backend staging

--- popup.js
+++ popup.js
@@ -15,123 +15,106 @@
 let myTabGroups = [];
 let currentTabs = [];
 
-class tabGroup
-{
-  name = "unsorted";
-  timestamp = 0;
-  tabList = [];
+class tabGroup {
+    name = "unsorted";
+    timestamp = 0;
+    tabList = [];
 
-  constructor(timestamp)
-  {
-    this.timestamp = timestamp;
-  }
+    constructor(timestamp) {
+        this.timestamp = timestamp;
+    }
 
-  setName(name) 
-  {
-    this.name = name;
-  }
+    setName(name) {
+        this.name = name;
+    }
 
-  addTab(tab)
-  {
-    this.tabList.push(tab);
-  }
+    addTab(tab) {
+        this.tabList.push(tab);
+    }
 
 }
 
 // Saves all the tabs in the current window
-async function getCurrentTabs() 
-{
-  await chrome.tabs.query({}).then((tabs) =>
-  {
-    for ( const tab of tabs) 
-    {
-      currentTabs.push(tab);
-    }
-  });
+async function getCurrentTabs() {
+    await chrome.tabs.query({}).then((tabs) => {
+        for (const tab of tabs) {
+            currentTabs.push(tab);
+        }
+    });
 }
 
-function storeUpdatedTabGroups()
-{
-  chrome.storage.sync.set({ "myTabGroups" : myTabGroups }, function() {
-    console.log("Storing everything for next time");
-    if (chrome.runtime.error) {
-      console.log("Runtime error.");
-    }
-  });
+function storeUpdatedTabGroups() {
+    chrome.storage.sync.set({ "myTabGroups": myTabGroups }, function () {
+        console.log("Storing everything for next time");
+        if (chrome.runtime.error) {
+            console.log("Runtime error.");
+        }
+    });
 }
 
-function restore()
-{
-  chrome.storage.sync.get("myTabGroups", values => {
-    if (!chrome.runtime.error)
-    {
-        console.log("restoring everything:");
-        console.log(values);
-        myTabGroups = values;
-        console.log(myTabGroups);
-    }
-});
+function restore() {
+    chrome.storage.sync.get("myTabGroups", values => {
+        if (!chrome.runtime.error) {
+            console.log("restoring everything:");
+            console.log(values);
+            myTabGroups = values;
+            console.log(myTabGroups);
+        }
+    });
 }
 
 // Looks at all the checked tabs in ist, and saves them in a "tabGroup" object in the "myTabGroups" list
-function saveSelectedTabs()
-{
-  console.log("Saving selected tabs");
-  var ts = new Date();
-  var tg = new tabGroup(ts);
-  var ul = document.getElementById('tabsToSaveList');
-  var items = ul.getElementsByTagName("li");
-  console.log(items);
-  for(var i=0; items[i]; ++i)
-  {
-      if(items[i].childNodes[1].checked == true)
-      {
-          console.log("saving "+currentTabs[i].title);
-          tg.addTab(currentTabs[i]);
-    
-      }
-  }
+function saveSelectedTabs() {
+    console.log("Saving selected tabs");
+    var ts = new Date();
+    var tg = new tabGroup(ts);
+    var ul = document.getElementById('tabsToSaveList');
+    var items = ul.getElementsByTagName("li");
+    console.log(items);
+    for (var i = 0; items[i]; ++i) {
+        if (items[i].childNodes[1].checked == true) {
+            console.log("saving " + currentTabs[i].title);
+            tg.addTab(currentTabs[i]);
+
+        }
+    }
     myTabGroups.push(tg);
     console.log(myTabGroups);
     storeUpdatedTabGroups();
 }
 
 
-function changeGroupName(sessionNumber, newName)
-{
-  myTabGroups[sessionNumber].setName(newName);
+function changeGroupName(sessionNumber, newName) {
+    myTabGroups[sessionNumber].setName(newName);
 }
 
-function loadTabFromGroup(sessionNumber, tabIndex)
-{
-  window.open(myTabGroups[sessionNumber].tabList[tabIndex]);
+function loadTabFromGroup(sessionNumber, tabIndex) {
+    window.open(myTabGroups[sessionNumber].tabList[tabIndex]);
 }
 
-function loadGroup(sessionNumber)
-{
-  for (i in myTabGroups[sessionNumber])
-  {
-    loadTabFromGroup(sessionNumber, i);
-  }
+function loadGroup(sessionNumber) {
+    for (i in myTabGroups[sessionNumber]) {
+        loadTabFromGroup(sessionNumber, i);
+    }
 }
 
 
-async function showTabsToSave()
-{
-  console.log("creating list of potential tabs to save");
-  await getCurrentTabs();
-  let list = document.getElementById("tabsToSaveList");
+async function showTabsToSave() {
+    console.log("creating list of potential tabs to save");
+    await getCurrentTabs();
+    let list = document.getElementById("tabsToSaveList");
 
-  currentTabs.forEach((i) =>{
-    console.log(i);
-    let li = document.createElement("li");
-    let inp = document.createElement("input");
-    inp.type = 'checkbox';
-    li.innerText = i.title;
-    li.appendChild(inp);
-    list.appendChild(li);
-  })
-  
+    currentTabs.forEach((i) => {
+        console.log(i);
+        let li = document.createElement("li");
+        let inp = document.createElement("input");
+        inp.type = 'checkbox';
+        li.innerText = i.title;
+        li.classList.add("saveList");
+        li.appendChild(inp);
+        list.appendChild(li);
+    })
+
 }
 
 // function showTabsToLoad()
@@ -153,7 +136,7 @@ async function showTabsToSave()
 
 // Reports errors to the console
 function onError(error) {
-  console.error(`Error: ${error}`);
+    console.error(`Error: ${error}`);
 }
 
 // Extension Starts here 
@@ -162,15 +145,15 @@ function onError(error) {
 const groupSelectedButton = document.getElementById("groupSelectedbtn");
 groupSelectedButton.addEventListener("click", saveSelectedTabs);
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
-  restore();
-  showTabsToSave();
+    restore();
+    showTabsToSave();
 
 });
 
-window.addEventListener('unload', function() {
+window.addEventListener('unload', function () {
 
-	// unregisterEvents();
-    
+    // unregisterEvents();
+
 });
