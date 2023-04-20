08323ff4997a56598cfff0529d1b0b055db081fe
paola <paolapolvera@gmail.com>
2023-04-19 20:33:02 -0500

    Copied backend files from Josh and Christians branch, to updated the UI

--- popup.js
+++ popup.js
@@ -28,32 +28,34 @@ async function getCurrentTabs() {
     });
 }
 
-function storeUpdatedTabGroups() {
-    // chrome.storage.sync.set({ "myTabGroups": myTabGroups }, function ()
-    // {
-    //     console.log("Storing everything for next time");
-    //     if (chrome.runtime.error) {
-    //         console.log("Runtime error.");
-    //     }
-    // });
-}
-
 function restore() {
-    chrome.storage.sync.get("myTabGroups", (values) => {
-        if (!chrome.runtime.error) {
-            console.log("restoring everything:");
-            console.log(values);
-            // myTabGroups = values;
+    chrome.storage.local.get('myTabGroups', function (data) {
+        if (data.myTabGroups) {
+            console.log('myTabGroups retrieved from Chrome storage:');
+            console.log(data.myTabGroups);
+            myTabGroups = data.myTabGroups;
             console.log(myTabGroups);
+        } else {
+            console.log('myTabGroups not found in Chrome storage');
         }
     });
 }
 
+
+function storeUpdatedTabGroups() {
+    chrome.storage.local.set({ 'myTabGroups': myTabGroups }, function () {
+        console.log('myTabGroups saved to Chrome storage');
+    });
+}
+
+
 // Looks at all the checked tabs in list, and saves them in a "tabGroup" object in the "myTabGroups" list
 function saveSelectedTabs() {
     console.log("saveSelectedTabs");
-    let ts = new Date();
-    let tg = new tabGroup(ts);
+    let ts = new Date().getTime();
+    let n = document.getElementById("groupNameInput").value;
+    console.log(n);
+    let tg = new tabGroup(ts, n);
     let ul = document.getElementById("tabsToSaveList");
     let items = ul.getElementsByTagName("li");
     for (let i = 0; items[i]; ++i) {
@@ -72,8 +74,9 @@ groupAllbtn.addEventListener("click", saveAllTabs);
 
 function saveAllTabs() {
     console.log("saveAllTabs");
-    var ts = new Date();
-    var tg = new tabGroup(ts);
+    var ts = new Date().getTime();
+    let n = document.getElementById("groupNameInput").value;
+    var tg = new tabGroup(ts, n);
     for (tab of currentTabs) {
         console.log(tab.title);
         tg.addTab(tab);
@@ -99,15 +102,31 @@ function loadTabFromGroup() {
     });
 }
 
-
 function loadGroup(group) {
-    console.log("LoadGroup");
+    console.log(group);
     for (t of group.tabList) {
         chrome.tabs.create({ url: t.url });
+        console.log(t.url);
     }
 }
 
-function DeleteGroup() { }
+function deleteGroup(group) {
+    const index = myTabGroups.indexOf(group);
+    if (index !== -1) {
+        console.log("deleted")
+        myTabGroups.splice(index, 1);
+        storeUpdatedTabGroups();
+        showTabsToLoad();
+    }
+}
+
+function editGroupName(group, input) {
+    const newName = input.value;
+    group.name = newName;
+    storeUpdatedTabGroups();
+    showTabsToLoad();
+}
+
 
 async function showTabsToSave() {
     console.log("creating list of potential tabs to save");
@@ -126,17 +145,94 @@ async function showTabsToSave() {
     });
 }
 
+
+var dropdown = document.getElementById("sortSelect");
+dropdown.addEventListener("change", handleDropdownSelection);
+
+function handleDropdownSelection() {
+    var dropdown = document.getElementById("sortSelect");
+    var selectedValue = dropdown.value;
+
+    if (selectedValue === "--Sort By--") {
+        console.log("--Sort By--");
+    } else if (selectedValue === "Alpha") {
+        console.log("Alpha");
+        sortTabGroupsByName();
+        showTabsToLoad();
+    } else if (selectedValue === "DateMade") {
+        console.log("DateMade");
+        sortTabGroupsByTimestamp();
+        showTabsToLoad();
+    }
+}
+
+function sortTabGroupsByName() {
+    myTabGroups.sort(function (a, b) {
+        var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+        var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+        if (nameA < nameB) {
+            return -1;
+        }
+        if (nameA > nameB) {
+            return 1;
+        }
+        // names must be equal
+        return 0;
+    });
+}
+
+function sortTabGroupsByTimestamp() {
+    myTabGroups.sort(function (a, b) {
+        console.log(b.timestamp);
+
+        return (b.timestamp) - (a.timestamp);
+
+    });
+}
+
+//just an example and not yet expanded
+function searchTabGroupsByName() {
+    var searchQuery = document.getElementById("searchInput").value.toUpperCase();
+    var searchResults = [];
+
+    for (var i = 0; i < myTabGroups.name.length; i++) {
+        var groupName = myTabGroups.name[i].toUpperCase();
+        if (groupName.includes(searchQuery)) {
+            searchResults.push(myTabGroups[i]);
+        }
+    }
+    // Display the search results in the popup window
+    // For example, you could display them in a table:
+    var tableBody = document.getElementById("searchResultsTableBody");
+    tableBody.innerHTML = "";
+    for (var j = 0; j < searchResults.length; j++) {
+        var row = tableBody.insertRow();
+        var nameCell = row.insertCell();
+        var urlsCell = row.insertCell();
+        nameCell.innerHTML = searchResults[j].name;
+        urlsCell.innerHTML = searchResults[j].urls.join(", ");
+    }
+}
+
+
+const showGroups = document.getElementById("tabsilver");
+showGroups.addEventListener("click", showTabsToLoad);
+
 function showTabsToLoad() {
-    console.log("showTabsToLoad called");
+    console.log(myTabGroups);
 
     // Get the tabsToLoadList unordered list
     const tabsToLoadList = document.getElementById("tabsToLoadList");
     // remove old list
     tabsToLoadList.innerHTML = "";
 
+
     // Loop through each list item in the tabsToLoadList
-    for (group of myTabGroups) {
-        console.log(group);
+    for (const group of myTabGroups) {
+
+        //console.log(group);
+        console.log(group.timestamp);
+
         const groupLi = document.createElement("li");
 
         const groupSpan = document.createElement("span");
@@ -144,10 +240,7 @@ function showTabsToLoad() {
         groupLabel = document.createElement("label");
         groupLabel.innerText = group.name;
         groupLabel.addEventListener("click", function () {
-            console.log("group clicked");
             loadGroup(group);
-            editBtn.style.display = "block";
-            deleteBtn.style.display = "block";
         });
         groupSpan.appendChild(groupLabel);
 
@@ -155,21 +248,27 @@ function showTabsToLoad() {
         groupDropDownBtn.setAttribute("class", "arrow-button");
         groupSpan.appendChild(groupDropDownBtn);
 
+        let input = document.createElement("input");
+        input.setAttribute("type", "text");
+        input.setAttribute("value", group.name);
+        input.style.display = "none";
+        groupSpan.appendChild(input);
+
         let editBtn = document.createElement("button");
         editBtn.innerText = "Edit";
         editBtn.id = "editBtn";
-        editBtn.addEventListener("click", changeGroupName);
+        editBtn.addEventListener("click", () => { editGroupName(group, input) });
         editBtn.style.display = "none";
         groupSpan.appendChild(editBtn);
-
-        groupLi.appendChild(groupSpan);
 
         let deleteBtn = document.createElement("button");
         deleteBtn.innerText = "Delete";
         deleteBtn.id = "deleteBtn";
         deleteBtn.style.display = "none";
-        deleteBtn.addEventListener("click", DeleteGroup); //DeleteGroup is not yet an existing function
+        deleteBtn.addEventListener("click", () => { deleteGroup(group) });
         tabsToLoadList.appendChild(deleteBtn);
+
+        groupLi.appendChild(groupSpan);
 
         tabsToLoadList.appendChild(groupLi);
         let groupUl = document.createElement("ul");
@@ -178,8 +277,8 @@ function showTabsToLoad() {
         groupUl.style.display = "none";
 
         for (tab of group.tabList) {
-            console.log(tab);
-            console.log("for tabs checklist list is loaded");
+            //console.log(tab);
+            //console.log("for tabs checklist list is loaded");
             let tabLi = document.createElement("li");
             let tabInp = document.createElement("input");
             tabInp.setAttribute("class", "tabCheckbox");
@@ -193,12 +292,18 @@ function showTabsToLoad() {
         }
 
         groupDropDownBtn.addEventListener("click", function () {
-            console.log("dropdownclicked");
+            //console.log("dropdownclicked");
             // Toggle the visibility of the dropdown list
             if (groupUl.style.display === "none") {
                 groupUl.style.display = "block";
+                editBtn.style.display = "block";
+                deleteBtn.style.display = "block";
+                input.style.display = "block";
             } else {
                 groupUl.style.display = "none";
+                editBtn.style.display = "none";
+                deleteBtn.style.display = "none";
+                input.style.display = "none";
             }
         });
 
